b0aa10442caebfecf7904fbd95d7dfe4cfa60ba1
mannno24 <127245655+mannno24@users.noreply.github.com>
2023-03-07 14:17:47 -0500

    Add files via upload

---
+++ intune.ps1
@@ -0,0 +1,4 @@
+﻿PowerShell.exe -ExecutionPolicy Bypass
+Install-Script -name Get-WindowsAutopilotInfo -Force
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+Get-WindowsAutopilotInfo -Online
